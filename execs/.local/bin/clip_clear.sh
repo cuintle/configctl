ad69c5e62c92e7802868f6acdd2d5ab857792fd0
cuintle <cuintledev@gmail.com>
2023-10-23 01:13:39 -0300

    minor improvements

--- execs/.local/bin/clip_clear.sh
+++ execs/.local/bin/clip_clear.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-wl-copy --clear
+CONTENT=$(wl-paste --primary)
 
-CONTENT=$(wl-paste)
+wl-copy --clear --primary
 
-[[ -z $CONTENT ]] && notify-send --app-name="Clipboard Clearer" --icon info --urgency normal "Clipboard Clearer" "Clipboard is Empty" || notify-send --app-name="Clipboard Clearer" --icon info --urgency normal "Clipboard Clearer" "Clipboard Content: $CONTENT"
+[[ -z $CONTENT ]] && notify-send --app-name="Clipboard Clearer" --icon info --urgency normal "Clipboard Clearer" "Clipboard is Empty" || notify-send --app-name="Clipboard Clearer" --icon info --urgency normal "Clipboard Clearer" "Clipboard Content was: $CONTENT, but it is now clear"
