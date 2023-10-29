47dd6359975a60369d6bae65dd5c00d28ba3ce7d
cuintle <cuintledev@gmail.com>
2023-10-28 21:59:06 -0300

    added workapps script

---
+++ execs/.local/bin/workapps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+workapps=(
+  "flatpak run org.gnome.Geary &"
+  "flatpak run org.qbittorrent.qBittorrent &"
+  "flatpak run com.bitwarden.desktop --ozone-platform-hint=auto --enable-gpu-rasterization --enable-zero-copy --enable-features=Vulkan &"
+  "flatpak run org.telegram.desktop &"
+  "flatpak run com.rtosta.zapzap &"
+  "flatpak run io.freetubeapp.FreeTube --ozone-platform-hint=auto --enable-gpu-rasterization --enable-zero-copy --enable-features=Vulkan &"
+  "flatpak run re.sonny.Tangram &"
+  "flatpak run com.raggesilver.BlackBox &"
+  "vivaldi-stable &"
+  "gnome-text-editor &"
+  "exec /home/cuintle/.local/bin/distrobox enter code -- env HOME=/home/cuintle/.local/share/distrobox_homes/Code XDG_CONFIG_HOME=/home/cuintle/.local/share/distrobox_homes/Code/.config code --enable-features=Vulkan --enable-gpu-rasterization --enable-zero-copy --unity-launch"
+)
+
+parallel --progress --jobs 3 --delay 3 --timeout 3600 --shuf "sh -c {}" ::: "${workapps[@]}"
